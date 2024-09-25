487587faa78f431def2326702451e4a3ae2e8248
Tyuiu.puzinada@outlook.com <dariapuzina7b@gmail.com>
2024-09-25 22:40:44 +0500

    Task6

---
+++ Tyuiu.PuzinaDA.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PuzinaDA.Sprint2.Task6.V1.Lib;
+namespace Tyuiu.PuzinaDA.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Пузина Д. А. | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Пузина Дарья Алексеевна | ИИПБ-24-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value;
+            string monthDays;
+            Console.Write("Введите номер месяца: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            if ((value < 1 )||( value > 12))
+            {
+                monthDays =  "Такого месяца нет, введите число от 1 до 12.";
+            }
+            else 
+            { 
+                monthDays = "В этом месяце " + ds.FindMonthDaysCount(value);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(monthDays);
+        }
+    }
+}
